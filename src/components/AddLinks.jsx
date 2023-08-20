b9147798db14ef502ce04492e64985f7d7447904
ioangheraszim <gijohny17@gmail.com>
2023-08-20 02:17:06 +0100

    finished link show component, need to add background image to link add component and i can start the other two remaining components

--- src/components/AddLinks.jsx
+++ src/components/AddLinks.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from "react";
 import "../scss/AddLinks.scss";
 const AddLinks = () => {
   return (
-    <section className=gi'add-link'>
-      <input type="text" placeholder='fuck this shit' />
-      <button>asdasdas</button>
-    </section>
-  )
-}
+    <>
+      <section className="add-link">
+        <div className="input-container">
+          <input
+            className="link-input"
+            type="text"
+            placeholder="Shorten link here..."
+          />
+          <label className="error-label">Please add a link</label>
+          <button className="short-btn">Shorten It!</button>
+        </div>
+      </section>
+      <section className="show-links">
+        <h2 className="long-url">http://127.0.0.1:50568/testing.html</h2>
+        <h2 className="short-url">https://rel.ink/k4lkyk</h2>
+        <button className="copy-btn">Copy</button>
+      </section>
+    </>
+  );
+};
 
-export default AddLinks
+export default AddLinks;
